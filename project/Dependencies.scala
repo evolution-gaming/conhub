3df524aca706b6305d5a4b9544a58e491ce413f5
Scala Steward <me@scala-steward.org>
2020-08-22 06:47:55 +0200

    Update scalatest to 3.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val nel                  = "com.evolutiongaming"        %% "nel"                % "1.3.4"
   val `safe-actor`         = "com.evolutiongaming"        %% "safe-actor"         % "3.0.0"
   val `scala-tools`        = "com.evolutiongaming"        %% "scala-tools"        % "3.0.5"
-  val scalatest            = "org.scalatest"              %% "scalatest"          % "3.2.1"
+  val scalatest            = "org.scalatest"              %% "scalatest"          % "3.2.2"
   val `scala-logging`      = "com.typesafe.scala-logging" %% "scala-logging"      % "3.9.2"
 
   object Akka {
