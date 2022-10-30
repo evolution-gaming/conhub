52029ebd8c3ba16e0e235e4627a7cfe44ffb0f93
Yaroslav Klymko <t3hnar@gmail.com>
2022-10-30 17:53:59 +0100

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,16 +4,16 @@ object Dependencies {
 
   val `config-tools`       = "com.evolutiongaming"        %% "config-tools"       % "1.0.4"
   val `future-helper`      = "com.evolutiongaming"        %% "future-helper"      % "1.0.6"
-  val sequentially         = "com.evolutiongaming"        %% "sequentially"       % "1.1.3"
+  val sequentially         = "com.evolutiongaming"        %% "sequentially"       % "1.1.4"
   val `akka-serialization` = "com.evolutiongaming"        %% "akka-serialization" % "1.0.4"
   val nel                  = "com.evolutiongaming"        %% "nel"                % "1.3.4"
   val `safe-actor`         = "com.evolutiongaming"        %% "safe-actor"         % "3.0.0"
   val `scala-tools`        = "com.evolutiongaming"        %% "scala-tools"        % "3.0.5"
-  val scalatest            = "org.scalatest"              %% "scalatest"          % "3.2.10"
-  val `scala-logging`      = "com.typesafe.scala-logging" %% "scala-logging"      % "3.9.4"
+  val scalatest            = "org.scalatest"              %% "scalatest"          % "3.2.14"
+  val `scala-logging`      = "com.typesafe.scala-logging" %% "scala-logging"      % "3.9.5"
 
   object Akka {
-    private val version = "2.6.8"
+    private val version = "2.6.19"
     val actor    = "com.typesafe.akka" %% "akka-actor"    % version
     val remote   = "com.typesafe.akka" %% "akka-remote"   % version
     val cluster  = "com.typesafe.akka" %% "akka-cluster"  % version
