70f06d3b6a8dfd05f812dacc51e4fc45eeecbc58
Eduard Dubrovsky <edubrovskii@evolution.com>
2023-06-27 16:28:12 +0200

    Rework UpdateResult

--- src/main/scala/com/evolutiongaming/conhub/UpdateResult.scala
+++ src/main/scala/com/evolutiongaming/conhub/UpdateResult.scala
@@ -1,24 +1,23 @@
 package com.evolutiongaming.conhub
 
-/**
-  *
-  * @param updated whether action did update the value
-  * @param value   value before the update if it was updated or current value otherwise
-  */
-final case class UpdateResult[+A](updated: Boolean = false, value: Option[A] = None)
+import com.evolutiongaming.conhub.UpdateResult.NotUpdated.Reason
+
+sealed trait UpdateResult[+C]
 
 object UpdateResult {
 
-  private val Empty = UpdateResult()
+  final case class Updated[C](previousValue: Option[C]) extends UpdateResult[C]
+  final case class NotUpdated[C](reason: Reason[C]) extends UpdateResult[C]
 
-  private val Created = UpdateResult(updated = true)
+  object NotUpdated {
+    sealed trait Reason[+C]
 
-
-  def empty[A]: UpdateResult[A] = Empty
-
-  def created[A]: UpdateResult[A] = Created
-
-  def apply[A](updated: Boolean, value: A): UpdateResult[A] = UpdateResult(updated, Some(value))
-
-  def apply[A](value: A): UpdateResult[A] = UpdateResult(value = Some(value))
+    object Reason {
+      final case object VersionConflict extends Reason[Nothing]
+      final case object SameValue extends Reason[Nothing]
+      final case object UpdateNotRequiredByMethod extends Reason[Nothing]
+      final case class UpdateNotDefinedForValue[C](previousValue: Option[C]) extends Reason[C]
+      final case class WrongContext[C](context: String, connection: C) extends Reason[C]
+    }
+  }
 }
